ef145f5181b0c9ffebd941e936ebc51271c4304f
PatricioRios <patricio_rios2010@live.com>
2024-05-16 15:36:50 -0300

    sin terminar

--- ejercicio13.c
+++ ejercicio13.c
@@ -18,14 +18,123 @@ typedef struct
 {
   char nombre[20];
   char apellido[20];
-  int telefono;
+  char telefono[20];
   char email[30];
 
-} Persona;
+} persona;
+
+int lastIndex = 0;
 
 int main()
 {
-  Persona *personas = malloc(10 * sizeof(Persona));
+  fflush(stdin);
+  char input[20];
+  int output = 0;
+  persona *personas = malloc(10 * sizeof(persona));
+  while (1 != 0)
+  {
+    printMenu(output);
+    scanf("%s", input);
+    // Ingresar
+    if (strcmp(input, "1") == 0)
+    {
+      addMenuOption(&personas, &output);
+      continue;
+    }
+    // eliminar
+    if (strcmp(input, "2") == 0)
+    {
+      continue;
+    }
+    // modificar
+    if (strcmp(input, "3") == 0)
+    {
+      break;
+    }
+    // exit
+    if (strcmp(input, ".exit") == 0)
+    {
+      printf("Invalid input.\n");
+    }
+  }
+}
 
-  whi
+void addMenuOption(persona *personas, int *output)
+{
+  fflush(stdin);
+  uiAddMenu();
+  logicAddMenu(personas, output);
+  printf("%s", *personas);
+}
+
+void logicAddMenu(persona *personas, int *output)
+{
+  uiAddMenu();
+  char firstChar = getchar();
+  if (firstChar == '-')
+  {
+    simpleAddPersonLogic(personas, output);
+  }
+  else
+  {
+    complexAddPersonLogic(personas, output, firstChar);
+  }
+}
+
+void simpleAddPersonLogic(persona *personas, int *output)
+{
+  scanf(
+      "%s,%s,%s,%s", personas[lastIndex].nombre, personas[lastIndex].apellido, personas[lastIndex].telefono, personas[lastIndex].email);
+  lastIndex++;
+
+  printPersons(personas);
+
+  *output = 1;
+}
+void printPersons(persona *personas)
+{
+  for (int i = 0; i > 5; i++)
+  {
+    printf("%s", personas[i]);
+  }
+}
+void complexAddPersonLogic(persona *personas, int *output, char firstChar)
+{
+  personas[lastIndex].nombre[0] = firstChar;
+
+  scanf("%s", personas[lastIndex].nombre[1]);
+  printf("|====================\n| PUT THE LAST NAME OF PERSON\n|====================\n");
+  scanf("%s", personas[lastIndex].apellido);
+  printf("|====================\n| PUT THE PHONE NUMBER OF PERSON\n|====================\n");
+  scanf("%s", personas[lastIndex].telefono);
+  printf("|====================\n| PUT THE EMAIL OF PERSON\n|====================\n");
+  scanf("%s", personas[lastIndex].email);
+  *output = 1;
+}
+
+void uiAddMenu()
+{
+  system("clear");
+  printf("|====================\n| Ingresa el nombre de la persona\n|====================\n| colocar '-nombre,apellido,telefono,email', para ingresar una persona mas rapido\n|====================\n");
+}
+
+// 0 for normal, 1 for success, 2 for error, 3 for exit
+void printMenu(int value)
+{
+  if (value == 0)
+  {
+    printf("|====================\n| Person Register\n|====================\n| 1. Registrar Persona\n|====================\n| 2. Registrar Persona\n|====================\n| 3. Registrar Persona\n|====================\n| Tipping '.exit' for exit the program\n|====================\n");
+  }
+  if (value == 1)
+  {
+    printf("|====================\n| Person Register\n|====================\n| 1. Registrar Persona\n|====================\n| 2. Registrar Persona\n|====================\n| 3. Registrar Persona\n|====================\n| Tipping '.exit' for exit the program\n|====================\n| TODO SALIO BIEN \n|====================\n");
+  }
+  if (value == 2)
+  {
+    printf("|====================\n| Person Register\n|====================\n| 1. Registrar Persona\n|====================\n| 2. Registrar Persona\n|====================\n| 3. Registrar Persona\n|====================\n| Tipping '.exit' for exit the program\n|====================\n| ALGO SALIO MAL... \n|====================\n");
+  }
+  if (value == 3)
+  {
+    printf("|====================\n| BYE BYE\n|====================\n");
+  }
 }
